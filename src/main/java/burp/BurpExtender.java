9be350323b8eacbb22a9bdda31fc6abda41a734e
isayan <raise.isayan@gmail.com>
2021-02-22 21:00:04 +0900

    bugfix

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -1,10 +1,6 @@
 package burp;
 
-import aspx.viewstate.IOptionProperty;
-import aspx.viewstate.OptionProperty;
 import extension.burp.BurpExtenderImpl;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.util.logging.Logger;
 import yagura.view.ViewStateDecoderTab;
 import yagura.view.ViewStateTab;
@@ -15,12 +11,12 @@ import yagura.view.ViewStateTab;
  */
 public class BurpExtender extends BurpExtenderImpl {
     private final static Logger logger = Logger.getLogger(BurpExtender.class.getName());
- 
+
     public BurpExtender() {
     }
 
     private final java.util.ResourceBundle BUNDLE = java.util.ResourceBundle.getBundle("burp/resources/release");
-    
+
     @SuppressWarnings("unchecked")
     public static BurpExtender getInstance() {
         return BurpExtenderImpl.<BurpExtender>getInstance();
@@ -28,37 +24,13 @@ public class BurpExtender extends BurpExtenderImpl {
 
     private final ViewStateTab viewStateTab = new ViewStateTab();
     private final ViewStateDecoderTab viewStateDecoderTab = new ViewStateDecoderTab();
-    
+
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
         super.registerExtenderCallbacks(callbacks);
         callbacks.setExtensionName(String.format("%s v%s", BUNDLE.getString("projname"), BUNDLE.getString("version")));
         callbacks.addSuiteTab(this.viewStateDecoderTab);
         callbacks.registerMessageEditorTabFactory(this.viewStateTab);
-    }
-
-    public PropertyChangeListener newPropertyChangeListener() {
-        return new PropertyChangeListener() {
-            @Override
-            public void propertyChange(PropertyChangeEvent evt) {
-                if (IOptionProperty.VIEW_STATE_PROPERTY.equals(evt.getPropertyName())) {
-                    applyOptionProperty();
-                }
-            }
-        };
-    }
-
-    private final OptionProperty option = new OptionProperty();
-
-    public OptionProperty getProperty() {
-        return this.option;
-    }
-
-    public void setProperty(IOptionProperty property) {
-        this.option.setProperty(property);
-    }
-
-    protected void applyOptionProperty() {
     }
 
 }
